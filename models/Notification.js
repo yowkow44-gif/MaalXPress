209d14a40610a77627df43ef9aceaac124f24f07
yowkow44-gif <yowkow44@gmail.com>
2025-12-19 20:46:43 +0530

    hahaha

---
+++ models/Notification.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const notificationSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      enum: ["success", "warning", "info", "default"],
+      default: "info",
+    },
+    isRead: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Notification", notificationSchema);
